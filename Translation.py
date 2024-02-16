40bde8d321690a8e5db1164848fa1361a06a375b
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-02-16 15:07:39 +0530

    Add print statements to display translated coordinates

--- Translation.py
+++ Translation.py
@@ -1,6 +1,11 @@
+# 2021UME4099 - Deepesh Kumar
+
 import matplotlib.pyplot as plt
 
 def translate_line(x1, y1, x2, y2, tx, ty):
+    print("Coordinates after Translation:")
+    print(f"A: ({x1+tx}, {y1+ty})")
+    print(f"B: ({x2+tx}, {y2+ty})")
     plt.plot([x1, x2], [y1, y2], label='Original line')
     plt.plot([x1+tx, x2+tx], [y1+ty, y2+ty], label='Line after translation')
     plt.legend()
@@ -15,9 +20,6 @@ def main():
     x2, y2 = map(int, input("Enter the ending point of line segment (x2 y2): ").split())
     tx, ty = map(int, input("Enter translation distances tx, ty: ").split())
     translate_line(x1, y1, x2, y2, tx, ty)
-    print("Coordinates after Translation:")
-    print(f"A: ({x1+tx}, {y1+ty})")
-    print(f"B: ({x2+tx}, {y2+ty})")
 
 if __name__ == "__main__":
     main()
